0f29f86e0b09925a9887d389cdfa2778af653e98
Yuki Walsh <Yuki.Walsh@Outlook.com>
2026-09-12 18:23:27 +1000

    allow_set_mouse_pos cvar

--- src/common/platform/posix/cocoa/i_input.mm
+++ src/common/platform/posix/cocoa/i_input.mm
@@ -42,6 +42,7 @@
 EXTERN_CVAR(Int, m_use_mouse)
 
 CVAR(Bool, use_mouse,    true,  CVAR_ARCHIVE | CVAR_GLOBALCONFIG)
+CVAR(Bool, allow_set_mouse_pos,    false,  CVAR_ARCHIVE | CVAR_GLOBALCONFIG)
 
 CVAR(Bool, k_allowfullscreentoggle, true, CVAR_ARCHIVE | CVAR_GLOBALCONFIG)
 
@@ -79,6 +80,11 @@ void SetCursorPosition(const NSPoint position)
 {
 	NSWindow* window = [NSApp keyWindow];
 	if (nil == window)
+	{
+		return;
+	}
+
+	if (!allow_set_mouse_pos)
 	{
 		return;
 	}
