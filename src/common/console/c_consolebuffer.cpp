a554761d5b7c6eef5fc84fdb6ba55cc6b3a5f1f3
Adam Kaminski <kaminskiadam9@gmail.com>
2026-09-16 21:47:47 -0400

    Fixed an old bug with the console buffer in which text with trailing newline characters appeared to be missing an extra line when drawn in the console.

--- src/common/console/c_consolebuffer.cpp
+++ src/common/console/c_consolebuffer.cpp
@@ -89,7 +89,11 @@ void FConsoleBuffer::AddText(PrintFlag printlevel, const char *text)
 	}
 	else if (text[textsize-1] == '\n')
 	{
-		textsize--;
+		// Don't remove the last newline if another one immediately precedes it.
+		// Otherwise, the text will be missing a line when it's shown in the console.
+		if (text[textsize-2] != '\n')
+			textsize--;
+
 		mAddType = NEWLINE;
 	}
 	else
